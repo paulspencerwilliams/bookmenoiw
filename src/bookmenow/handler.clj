0f97aeb1c38d25d4c92987cdbd0348638d797e32
Paul Williams <paul@paulswilliams.me.uk>
2017-01-02 16:03:04 +0000

    lein new compojure-app bookmenow

---
+++ src/bookmenow/handler.clj
@@ -0,0 +1,23 @@
+(ns bookmenow.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [bookmenow.routes.home :refer [home-routes]]))
+
+(defn init []
+  (println "bookmenow is starting"))
+
+(defn destroy []
+  (println "bookmenow is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
